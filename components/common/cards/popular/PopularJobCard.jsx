46d8943b63b81bdf69b578edc1fce0a80d9c724a
ujwal <`ujwal.yangalwar@decimalpointanalytics.com>
2023-10-21 02:11:18 +0530

    handled nearby cards

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,17 +1,13 @@
-import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./popularjobcard.style";
-
-import { checkImageURL } from '../../../../utils/index'
+import { checkImageURL } from "../../../../utils/index";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
     <TouchableOpacity
       style={styles.container(selectedJob, item)}
-      onPress={() => {
-        handleCardPress(item);
-      }}
+      onPress={() => handleCardPress(item)}
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
@@ -29,8 +25,15 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       </Text>
 
       <View style={styles.infoContainer}>
-        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>{item.job_title}</Text>
-        <Text style={styles.location}>{item.job_country}</Text>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <View style={styles.infoWrapper}>
+          <Text style={styles.publisher(selectedJob, item)}>
+            {item?.job_publisher} -
+          </Text>
+          <Text style={styles.location}> {item.job_country}</Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
